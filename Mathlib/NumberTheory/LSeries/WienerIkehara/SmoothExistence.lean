43c9657dbf8c40a4a42af67e8858986baa568408
Terence Tao <teorth@gmail.com>
2026-08-19 00:10:20 -0700

    golf

--- Mathlib/NumberTheory/LSeries/WienerIkehara/SmoothExistence.lean
+++ Mathlib/NumberTheory/LSeries/WienerIkehara/SmoothExistence.lean
@@ -29,28 +29,19 @@ end Function
 open MeasureTheory Set Real Function
 open scoped ContDiff
 
-lemma smooth_urysohn_support_Ioo {a b c d : ℝ} (h1 : a < b) (h3 : c < d) :
+lemma smooth_urysohn_support_Ioo {a b c d : ℝ} (hab : a < b) (hcd : c < d) :
     ∃ Ψ : ℝ → ℝ, (ContDiff ℝ ∞ Ψ) ∧ (HasCompactSupport Ψ) ∧
     indicator (Icc b c) 1 ≤ Ψ ∧ Ψ ≤ indicator (Ioo a d) 1 ∧
     (support Ψ = Ioo a d) := by
-  obtain ⟨Ψ, hΨSmooth, hΨrange, hΨ0, hΨ1⟩ :=
-    exists_contMDiff_zero_iff_one_iff_of_isClosed
+  obtain ⟨Ψ, hΨSmooth, hΨrange, hΨ0, _⟩ := exists_contMDiff_zero_iff_one_iff_of_isClosed
     (modelWithCornersSelf ℝ ℝ) (s := Iic a ∪ Ici d) (t := Icc b c)
-    (IsClosed.union isClosed_Iic isClosed_Ici) isClosed_Icc
-    (by grind)
-  simp only [mem_union, mem_Iic, mem_Ici, mem_Icc, range_subset_iff] at *
-  refine ⟨Ψ, ContMDiff.contDiff hΨSmooth, ?_, ?_, ?_, ?_⟩
-  · apply HasCompactSupport.of_support_subset_isCompact (K := Set.Icc a d) isCompact_Icc
-    simp
-    grind
-  · exact indicator_le' (by simp; grind) fun x _ ↦ (hΨrange x).1
-  · refine fun x ↦ le_indicator_apply (fun _ ↦ (hΨrange x).2) ?_
-    intro hx
-    rw [← hΨ0 x |>.mp]
-    simpa [-not_and, not_and_or] using hx
-  · ext
-    simp [← hΨ0]
-
+    (isClosed_Iic.union isClosed_Ici) isClosed_Icc (by grind)
+  simp only [range_subset_iff] at *
+  refine ⟨Ψ, hΨSmooth.contDiff,
+    HasCompactSupport.of_support_subset_isCompact (K := Icc a d) isCompact_Icc (by simp; grind),
+    indicator_le' (by simp; grind) fun x _ ↦ (hΨrange x).1,
+    fun x ↦ le_indicator_apply (fun _ ↦ (hΨrange x).2) (fun hx ↦ by grind),
+    by ext; simp [← hΨ0]⟩
 
 lemma SmoothExistence :
     ∃ (ν : ℝ → ℝ), (ContDiff ℝ ∞ ν) ∧ (∀ x, 0 ≤ ν x) ∧
@@ -66,26 +57,23 @@ lemma SmoothExistence :
   obtain ⟨ν, hνContDiff, _, hν0, hν1, hνSupport⟩ :=
     smooth_urysohn_support_Ioo (a := 1 / 2) (b := 1) (c := 3 / 2) (d := 2)
     (by linarith) (by linarith)
-  use ν, hνContDiff
   unfold indicator at hν0 hν1
-  split_ands
-  · exact fun x ↦ le_trans (by simp [apply_ite]) (hν0 x)
-  · grind
-  · rw [integral_pos_iff_support_of_nonneg]
-    · have : (Ioo (1 / 2 : ℝ) 2 ∩ {0}ᶜ ∩ Ici 0) = Ioo (1 / 2) 2 := by
-        grind
-      simp
-      grind [support_id, volume_Ioo, ENNReal.ofReal_pos]
-    · simp_rw [Pi.le_def, Pi.zero_apply]
-      intro y
-      by_cases h : y ∈ support ν
-      · apply div_nonneg <| le_trans (by simp [apply_ite]) (hν0 y)
-        grind
-      · simp only [mem_support, ne_eq, not_not] at h
-        simp [h]
-    · have : (fun x ↦ ν x / x).support ⊆ Icc (1 / 2) 2 := by
-        rw [support_div, hνSupport]
-        exact (inter_subset_left).trans Ioo_subset_Icc_self
-      apply (integrableOn_iff_integrable_of_support_subset this).mp
-      apply ContinuousOn.integrableOn_compact isCompact_Icc
-      apply hνContDiff.continuous.continuousOn.div continuousOn_id (by grind)
+  refine ⟨ν, hνContDiff, fun x ↦ le_trans (by simp [apply_ite]) (hν0 x), by grind, ?_ ⟩
+  rw [integral_pos_iff_support_of_nonneg]
+  · have : (Ioo (1 / 2 : ℝ) 2 ∩ {0}ᶜ ∩ Ici 0) = Ioo (1 / 2) 2 := by
+      grind
+    simp
+    grind [support_id, volume_Ioo, ENNReal.ofReal_pos]
+  · simp_rw [Pi.le_def, Pi.zero_apply]
+    intro y
+    by_cases h : y ∈ support ν
+    · apply div_nonneg <| le_trans (by simp [apply_ite]) (hν0 y)
+      grind
+    · simp only [mem_support, ne_eq, not_not] at h
+      simp [h]
+  · have : (fun x ↦ ν x / x).support ⊆ Icc (1 / 2) 2 := by
+      rw [support_div, hνSupport]
+      exact (inter_subset_left).trans Ioo_subset_Icc_self
+    apply (integrableOn_iff_integrable_of_support_subset this).mp
+    apply ContinuousOn.integrableOn_compact isCompact_Icc
+    apply hνContDiff.continuous.continuousOn.div continuousOn_id (by grind)
